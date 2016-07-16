043ad10bce5143fd943ac8f4375b7cbbf196ef20
Igor Vaz <igorvaz@unicastconsultoria.com.br>
2016-07-16 10:14:40 -0300

    corrigindo criacao

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import SGDClassifier
 
-sdgs = []
+sgds = []
 
 #cria os 100 classificadores 
 for i in range(0,100):
-	sgds[i] = SGDClassifier(loss="hinge", alpha=0.01, n_iter=200, fit_intercept=True)
+	sgds.append(SGDClassifier(loss="hinge", alpha=0.01, n_iter=200, fit_intercept=True))
